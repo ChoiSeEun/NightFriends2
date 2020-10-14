7523f2b609504ef6a40a46f0390bdbc435705d9a
Sung A <53103434+sunga08@users.noreply.github.com>
2020-10-14 23:28:10 +0900

    class import

--- app/src/main/java/com/example/night_friend/main_map/Fragment4_search.java
+++ app/src/main/java/com/example/night_friend/main_map/Fragment4_search.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import android.view.inputmethod.InputMethodManager;
 
 import com.example.night_friend.R;
 import com.example.night_friend.matching.BaseAdapterSearch;
