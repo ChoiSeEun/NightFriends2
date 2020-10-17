361980c6ec6515f88fcc4b2b65fdc457c82edec1
Sung A <53103434+sunga08@users.noreply.github.com>
2020-10-17 22:04:11 +0900

    Update Fragment4_search.java
    class import

--- app/src/main/java/com/example/night_friend/main_map/Fragment4_search.java
+++ app/src/main/java/com/example/night_friend/main_map/Fragment4_search.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
