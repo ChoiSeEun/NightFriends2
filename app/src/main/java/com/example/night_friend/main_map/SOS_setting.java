d9095bf0d267dad6ba5cc19826737943267cafb8
Sung A <53103434+sunga08@users.noreply.github.com>
2020-10-27 12:13:13 +0900

    Update SOS_setting.java
    - 긴급신고 설정과 기능 연결 
     (112신고 => 체크시 112 신고, 체크 안할시 첫번째 연락처로 전화 /  모두에게 연락 => 체크시 저장된 번호 모두에게 sms 발송, 체크 안할시 첫번째 연락처로만 sms 발송)

--- app/src/main/java/com/example/night_friend/main_map/SOS_setting.java
+++ app/src/main/java/com/example/night_friend/main_map/SOS_setting.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -18,14 +19,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.night_friend.R;
 
 public class SOS_setting extends AppCompatActivity {
-    CheckBox cb_sos,cb_112,cb_all;
+    CheckBox cb_112,cb_all;
     EditText name1, name2, name3, name4;
     public static EditText phone1;
     EditText phone2, phone3, phone4;
     public static Context context_setting;
     public String ph1,ph2,ph3,ph4;
     public static boolean autoSwitchState;
-
+    public static boolean sosSwitchState;
+    public static boolean callPoliceState;
+    public static boolean callAllState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +43,20 @@ public class SOS_setting extends AppCompatActivity {
         cb_112=findViewById(R.id.cb_112);
         cb_all=findViewById(R.id.cb_all);
 
-        ImageButton add1=(ImageButton)findViewById(R.id.bt_guide_search);
-        ImageButton add2=(ImageButton)findViewById(R.id.bt_refresh);
-        ImageButton add3=(ImageButton)findViewById(R.id.bt_cancle);
-        ImageButton add4=(ImageButton)findViewById(R.id.imageButton5);
+        ImageButton add1=(ImageButton)findViewById(R.id.bt_add1);
+        ImageButton add2=(ImageButton)findViewById(R.id.bt_add2);
+        ImageButton add3=(ImageButton)findViewById(R.id.bt_add3);
+        ImageButton add4=(ImageButton)findViewById(R.id.bt_add4);
 
-        name1=(EditText)findViewById(R.id.editText);
-        name2=(EditText)findViewById(R.id.editText5);
-        name3=(EditText)findViewById(R.id.editText6);
-        name4=(EditText)findViewById(R.id.editText10);
+        name1=(EditText)findViewById(R.id.set_name1);
+        name2=(EditText)findViewById(R.id.set_name2);
+        name3=(EditText)findViewById(R.id.set_name3);
+        name4=(EditText)findViewById(R.id.set_name4);
 
-        phone1=(EditText)findViewById(R.id.editText4);
-        phone2=(EditText)findViewById(R.id.editText8);
-        phone3=(EditText)findViewById(R.id.editText9);
-        phone4=(EditText)findViewById(R.id.editText11);
+        phone1=(EditText)findViewById(R.id.set_phone1);
+        phone2=(EditText)findViewById(R.id.set_phone2);
+        phone3=(EditText)findViewById(R.id.set_phone3);
+        phone4=(EditText)findViewById(R.id.set_phone4);
 
         SharedPreferences sp=getSharedPreferences("sfile",MODE_PRIVATE);
         String n1=sp.getString("name1","");
@@ -76,51 +79,108 @@ public class SOS_setting extends AppCompatActivity {
         phone3.setText(save3);
         phone4.setText(save4);
 
+        //SharedPreferences settings = getSharedPreferences("switch", 0);
+
         SharedPreferences sharedPreferences =getSharedPreferences("sfile",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
 
-        autoSwitchState = sharedPreferences.getBoolean("switchkey", true);
+        autoSwitchState = sharedPreferences.getBoolean("autoSwitch", true);
+        sosSwitchState = sharedPreferences.getBoolean("sosSwitch", false);
+        callPoliceState = sharedPreferences.getBoolean("callPoliceSwitch", false);
+        callAllState = sharedPreferences.getBoolean("callAllSwitch", false);
         autoSwitch.setChecked(autoSwitchState);
+        sosSwitch.setChecked(sosSwitchState);
+        cb_112.setChecked(callPoliceState);
+        cb_112.setEnabled(sosSwitchState);
+        cb_all.setChecked(callAllState);
+        cb_all.setEnabled(sosSwitchState);
+        Log.e("auto switch: ",""+autoSwitchState);
+        Log.e("112 switch: ",""+callPoliceState);
+        Log.e("all switch: ",""+callAllState);
 
+        //autoSwitch.setChecked(true);
+        //자동신고 기능 스위치
         autoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent intent = new Intent(getApplicationContext(), Night_main.class);
+                //SharedPreferences sharedPreferences = getSharedPreferences("sfile",MODE_PRIVATE);
+                //SharedPreferences.Editor editor=sharedPreferences.edit();
+
                 if(isChecked){
                     Toast.makeText(getApplicationContext(),"자동신고 기능 on",Toast.LENGTH_LONG).show();
+                    //intent.putExtra("auto_report", true);
                     autoSwitchState=true;
-                    editor.putBoolean("switchkey", true);
+                    editor.putBoolean("autoSwitch", true);
                     editor.commit();
+
                 }else{
                     Toast.makeText(getApplicationContext(),"자동신고 기능 off",Toast.LENGTH_LONG).show();
+                    //intent.putExtra("auto_report", false);
                     autoSwitchState=false;
-                    editor.putBoolean("switchkey", false);
+                    editor.putBoolean("autoSwitch", false);
                     editor.commit();
                 }
+                //SharedPreferences settings = getSharedPreferences("switch", 0);
+                //SharedPreferences.Editor editor = settings.edit();
             }
         });
 
+        //긴급신고 기능 스위치
         sosSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    Toast.makeText(getApplicationContext(),"switch on",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"긴급신고 기능 on",Toast.LENGTH_LONG).show();
+                    //cb_sos.setEnabled(true);
                     cb_112.setEnabled(true);
                     cb_all.setEnabled(true);
+                    sosSwitchState = true;
+                    editor.putBoolean("sosSwitch", true);
+                    editor.commit();
                 }else{
-                    Toast.makeText(getApplicationContext(),"switch off",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"긴급신고 off",Toast.LENGTH_LONG).show();
+                    //cb_sos.setEnabled(false);
                     cb_112.setEnabled(false);
+                    cb_112.setChecked(false);
                     cb_all.setEnabled(false);
+                    cb_all.setChecked(false);
+                    sosSwitchState = false;
+                    callPoliceState = false;
+                    callAllState = false;
+                    editor.putBoolean("sosSwitch", false);
+                    editor.putBoolean("callPoliceSwitch", false);
+                    editor.putBoolean("callAllSwitch", false);
+                    editor.commit();
                 }
             }
         });
+
+
+        /*cb_sos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(cb_sos.isChecked()==true){
+                    Toast.makeText(SOS_setting.this,"sos 사용: "+cb_sos.isChecked(), Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(SOS_setting.this,"sos 사용: "+cb_sos.isChecked(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });*/
 
         cb_112.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(cb_112.isChecked()==true){
                     Toast.makeText(SOS_setting.this,"112 신고: "+cb_112.isChecked(), Toast.LENGTH_LONG).show();
+                    callPoliceState = true;
+                    editor.putBoolean("callPoliceSwitch", true);
+                    editor.commit();
                 }else{
                     Toast.makeText(SOS_setting.this,"112 신고: "+cb_112.isChecked(), Toast.LENGTH_LONG).show();
+                    callPoliceState = false;
+                    editor.putBoolean("callPoliceSwitch", false);
+                    editor.commit();
                 }
             }
         });
@@ -130,13 +190,19 @@ public class SOS_setting extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(cb_all.isChecked()==true){
                     Toast.makeText(SOS_setting.this,"모두에게 신고: "+cb_all.isChecked(), Toast.LENGTH_LONG).show();
+                    callAllState = true;
+                    editor.putBoolean("callAllSwitch", true);
+                    editor.commit();
                 }else{
                     Toast.makeText(SOS_setting.this,"모두에게 신고: "+cb_all.isChecked(), Toast.LENGTH_LONG).show();
+                    callAllState = false;
+                    editor.putBoolean("callAllSwitch", false);
+                    editor.commit();
                 }
             }
         });
 
-        //연락처 추가
+        //추가 버튼
         add1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
