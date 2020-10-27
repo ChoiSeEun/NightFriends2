7be9ed6b7e924375b10d445e691fee0f843a138f
ChoiSeEun <48899016+ChoiSeEun@users.noreply.github.com>
2020-10-27 20:50:50 +0900

    gallery 수정

--- app/src/main/java/com/example/night_friend/main_map/SOS_gallery.java
+++ app/src/main/java/com/example/night_friend/main_map/SOS_gallery.java
@@ -8,6 +8,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,26 +31,33 @@ public class SOS_gallery extends AppCompatActivity {
         adapter = new com.example.night_friend.main_map.GalleryListViewAdapter();
         listView.setAdapter(adapter);
         //저장된 동영상 파일 이름 다 가져오기
-        /*String path = Environment.getExternalStorageDirectory().getAbsolutePath()+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+
                 File.separator+"List"+File.separator;
         File list = new File(path);
-        String [] List = list.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                Boolean bOK = false;
-                if(name.toLowerCase().endsWith(".mp4")) bOK =true;
-                //if(name.toLowerCase().endsWith(".mp3")) bOK = true;
-               return bOK;
+        if(list==null){
+            Toast.makeText(SOS_gallery.this,"저장된 동영상이 없습니다.",Toast.LENGTH_LONG).show();
+        }
+        else{
+            String [] List = list.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    Boolean bOK = false;
+                    if(name.toLowerCase().endsWith(".mp4")) bOK =true;
+                    //if(name.toLowerCase().endsWith(".mp3")) bOK = true;
+                    return bOK;
+                }
+            });
+            if(List!=null){
+                for (int i=0;i<List.length;i++) {
+                    try {
+                        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(List[i], MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
+                        Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap, 360, 480);
+                        adapter.addItem(thumbnail, List[i], "road", "date");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        });
-        for (int i=0;i<List.length;i++) {
-            try {
-                Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(List[i], MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
-                Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap, 360, 480);
-                adapter.addItem(thumbnail, List[i], "road", "date");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }*/
+        }
     }
 }
